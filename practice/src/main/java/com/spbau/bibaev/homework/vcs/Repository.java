0820187a4a13b284b9609be6f8c0f08af9e50027
roenke <roenke54@gmail.com>
2016-09-15 18:56:50 +0300

    Open nearest repository in current directory or parents

--- practice/src/main/java/com/spbau/bibaev/homework/vcs/Repository.java
+++ practice/src/main/java/com/spbau/bibaev/homework/vcs/Repository.java
@@ -1,34 +1,58 @@
 package com.spbau.bibaev.homework.vcs;
 
+import com.spbau.bibaev.homework.vcs.util.FilesUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
 public class Repository {
-  private static final String VCS_DIRECTORY_SUFFIX = File.separator + ".my_vcs" + File.separator;
+  private static final String VCS_DIRECTORY_NAME = ".my_vcs";
 
-  private Repository(@NotNull File directory) {
-    String repPath = directory.getAbsolutePath() + VCS_DIRECTORY_SUFFIX;
+  /**
+   * Open nearest repository (may be in parent folders) or open non-initialized here
+   *
+   * @param directory Start directory
+   * @return Existed repository, if it exists, otherwise empty non-initialized repository
+   */
+  @NotNull
+  public static Repository open(@NotNull File directory) {
+    File currentDirectory = directory;
+    while (currentDirectory != null && !FilesUtil.isContainsDirectory(currentDirectory, VCS_DIRECTORY_NAME)) {
+      currentDirectory = currentDirectory.getParentFile();
+    }
+
+    File vcsRoot = currentDirectory == null ? directory : currentDirectory;
+    return openHere(vcsRoot);
   }
 
-  /** Check that metadata created
-   * @return true, if metadate already created, false otherwise
+  /** Open/create repository here
+   * @param directory directory for repository creating
+   * @return repository
+   */
+  public static Repository openHere(@NotNull File directory) {
+    return new Repository(directory);
+  }
+
+  private Repository(@NotNull File directory) {
+
+  }
+
+  /**
+   * Check that metadata created
+   *
+   * @return true, if metadata already created, false otherwise
    */
   public boolean isInitialized() {
     return false;
   }
 
-
-  /** Create metadata files for current directory
+  /**
+   * Create metadata files for current directory
+   *
    * @return true, if metadata successfully created, false if it already exists
    */
   public boolean initialize() {
     return false;
-  }
-
-  @NotNull
-  public static Repository open(@NotNull File projectDirectory) {
-    return new Repository(projectDirectory);
   }
 
   private class Storage {

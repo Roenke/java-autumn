a1c7a2af9f4bfad1c747dd0fec14caa552fd6848
roenke <roenke54@gmail.com>
2016-09-20 20:57:44 +0300

    Add basic implementation for merge command

--- practice/src/main/java/com/spbau/bibaev/homework/vcs/command/impl/MergeCommand.java
+++ practice/src/main/java/com/spbau/bibaev/homework/vcs/command/impl/MergeCommand.java
@@ -2,20 +2,39 @@ package com.spbau.bibaev.homework.vcs.command.impl;
 
 import com.spbau.bibaev.homework.vcs.command.RepositoryCommand;
 import com.spbau.bibaev.homework.vcs.ex.RepositoryException;
+import com.spbau.bibaev.homework.vcs.repository.Branch;
 import com.spbau.bibaev.homework.vcs.repository.Repository;
+import com.spbau.bibaev.homework.vcs.repository.Revision;
+import com.spbau.bibaev.homework.vcs.util.ConsoleColoredPrinter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.List;
 
 public class MergeCommand extends RepositoryCommand {
+
   public MergeCommand(@NotNull File directory) {
     super(directory);
   }
 
+  interface MergeStrategy {
+    @NotNull
+    Revision merge(@NotNull Revision from, @NotNull Revision into) throws RepositoryException;
+  }
+
   @Override
   protected void perform(@NotNull List<String> args, @NotNull Repository repository) throws RepositoryException {
+    Branch srcBranch = repository.getBranchByName(args.get(0));
 
+    if(srcBranch == null) {
+      ConsoleColoredPrinter.println("Such branch not found", ConsoleColoredPrinter.RED);
+      return;
+    }
+
+    Branch dstBranch = repository.getCurrentBranch();
+    MergeStrategy mergeStrategy = new MyPrimitiveMergeStrategy();
+    mergeStrategy.merge(srcBranch.getLastRevision(), dstBranch.getLastRevision());
+    ConsoleColoredPrinter.println("Successfully");
   }
 
   @Override
@@ -31,5 +50,13 @@ public class MergeCommand extends RepositoryCommand {
   @Override
   protected int getMaxArgCount() {
     return 1;
+  }
+
+  private static class MyPrimitiveMergeStrategy implements MergeStrategy {
+    @Override
+    @NotNull
+    public Revision merge(@NotNull Revision from, @NotNull Revision into) {
+      return null;
+    }
   }
 }

311de2cf7c3d448efa852a6b4b973770931460f7
roenke <roenke54@gmail.com>
2016-10-30 18:34:11 +0300

    Use the final modifier for ProtocolDetail constants

--- practice/src/main/java/homework/ftp/common/ProtocolDetail.java
+++ practice/src/main/java/homework/ftp/common/ProtocolDetail.java
@@ -1,14 +1,14 @@
 package homework.ftp.common;
 
 public class ProtocolDetail {
-  public static int LIST_ACTION_ID = 1;
-  public static int GET_ACTION_ID = 2;
+  public static final int LIST_ACTION_ID = 1;
+  public static final int GET_ACTION_ID = 2;
 
-  public static int DEFAULT_PORT_NUMBER = 32456;
+  public static final int DEFAULT_PORT_NUMBER = 32456;
 
   public static class ErrorCodes {
-    public static long SUCH_FILE_NOT_FOUND = -1;
-    public static long SUCH_DIRECTORY_NOT_FOUND = -2;
-    public static long ACCESS_DENIED = -3;
+    public static final long SUCH_FILE_NOT_FOUND = -1;
+    public static final long SUCH_DIRECTORY_NOT_FOUND = -2;
+    public static final long ACCESS_DENIED = -3;
   }
 }

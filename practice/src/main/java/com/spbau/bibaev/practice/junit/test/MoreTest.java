5ec48d6b776949a9ecfb818f45273ea357650d88
Vitaliy Bibaev <roenke54@gmail.com>
2016-11-02 13:06:41 +0300

    Add simple implementation for the test-runner

---
+++ practice/src/main/java/com/spbau/bibaev/practice/junit/test/MoreTest.java
@@ -0,0 +1,14 @@
+package com.spbau.bibaev.practice.junit.test;
+
+import com.spbau.bibaev.practice.junit.annotation.Test;
+
+public class MoreTest {
+  @Test
+  public void test1() {
+
+  }
+
+  public void test2() {
+
+  }
+}

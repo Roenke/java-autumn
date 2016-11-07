183ff6d41cbf0d30184e34808492aae830130097
Vitaliy Bibaev <roenke54@gmail.com>
2016-11-07 10:32:39 +0300

    Implement upload handler

--- practice/src/main/java/com/spbau/bibaev/homework/torrent/server/handler/UploadHandler.java
+++ practice/src/main/java/com/spbau/bibaev/homework/torrent/server/handler/UploadHandler.java
@@ -1,8 +1,11 @@
 package com.spbau.bibaev.homework.torrent.server.handler;
 
+import com.spbau.bibaev.homework.torrent.common.FileInfo;
 import com.spbau.bibaev.homework.torrent.server.ServerStateEx;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -12,6 +15,14 @@ import java.net.Socket;
 public class UploadHandler extends RequestHandler {
   @Override
   protected void handleImpl(@NotNull Socket socket, @NotNull ServerStateEx serverState) throws IOException {
-
+    try (DataInputStream is = new DataInputStream(socket.getInputStream())) {
+      String name = is.readUTF();
+      long size = is.readLong();
+      FileInfo info = new FileInfo(name, size);
+      int newFileId = serverState.addNewFile(info);
+      try (DataOutputStream os = new DataOutputStream(socket.getOutputStream())) {
+        os.writeInt(newFileId);
+      }
+    }
   }
 }

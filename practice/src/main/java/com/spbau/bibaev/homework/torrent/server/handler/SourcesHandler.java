32aec35b3ce3d2bd0757bfcbd721cd0635807cdf
roenke <roenke54@gmail.com>
2016-11-07 03:23:08 +0300

    Add handler for source request

--- practice/src/main/java/com/spbau/bibaev/homework/torrent/server/handler/SourcesHandler.java
+++ practice/src/main/java/com/spbau/bibaev/homework/torrent/server/handler/SourcesHandler.java
@@ -1,5 +1,6 @@
 package com.spbau.bibaev.homework.torrent.server.handler;
 
+import com.spbau.bibaev.homework.torrent.server.ClientInfo;
 import com.spbau.bibaev.homework.torrent.server.ServerState;
 import org.jetbrains.annotations.NotNull;
 
@@ -7,6 +8,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * @author Vitaliy.Bibaev
@@ -15,9 +18,22 @@ public class SourcesHandler extends ReadOnlyRequestHandler {
   @Override
   void handleReadOnlyRequest(@NotNull Socket clientSocket, @NotNull ServerState serverState) throws IOException {
     try (DataInputStream is = new DataInputStream(clientSocket.getInputStream())) {
-      final int id = is.readInt();
-      try(DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream())) {
-        
+      final int targetFileId = is.readInt();
+      try (DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream())) {
+        final Collection<ClientInfo> clients = serverState.getConnectedClients();
+
+        Collection<ClientInfo> clientsWithTargetFile = clients.stream()
+            .filter(client -> serverState.getFilesByClient(client).contains(targetFileId))
+            .collect(Collectors.toList());
+
+        out.writeInt(clientsWithTargetFile.size());
+        for (ClientInfo client : clientsWithTargetFile) {
+          for (byte b : client.getIp()) {
+            out.writeByte(b);
+          }
+
+          out.writeShort(client.getPort());
+        }
       }
     }
   }

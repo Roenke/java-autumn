5cd13a81cc211c878541f4fbc3baf86ea0e3693d
roenke <roenke54@gmail.com>
2017-01-15 14:44:00 +0300

    Refactoring after review in entry point

--- practice/src/main/java/com/spbau/bibaev/homework/vcs/EntryPoint.java
+++ practice/src/main/java/com/spbau/bibaev/homework/vcs/EntryPoint.java
@@ -5,6 +5,7 @@ import com.spbau.bibaev.homework.vcs.command.CommandFactory;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,29 +20,27 @@ import java.util.stream.Collectors;
  * status
  * checkout branch_name|revision_hash
  * reset [file] {revert file or revert all files to current revision status}
- * TODO: merge branch {merge branch into current branch}
- *
- * TODO: add exceptions
- *
+ * <p>
  * Solutions:
  * Store only diff for each commit.
  */
 public class EntryPoint {
-  public static String VCS_NAME = "vcs";
+  public static final String VCS_NAME = "vcs";
+
   public static void main(String[] args) {
     if (args.length == 0) {
       usage();
       return;
     }
 
-    Path currentDirectory = new File(System.getProperty("user.dir")).toPath();
-    Command command = CommandFactory.createCommand(currentDirectory, args[0]);
+    final Path currentDirectory = Paths.get("user.dir");
+    final Command command = CommandFactory.createCommand(currentDirectory, args[0]);
     if (command == null) {
       usage();
       return;
     }
 
-    List<String> commandArgs = Arrays.stream(args).skip(1).collect(Collectors.toList());
+    final List<String> commandArgs = Arrays.asList(args).subList(1, args.length);
     command.perform(commandArgs);
   }
 
